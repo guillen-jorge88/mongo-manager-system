6af2717d2271fa33078d8cdc0fcc6ad59d92704b
guillen-jorge88 <guillen.jorge88@gmail.com>
2020-10-14 11:16:00 -0500

    create modules (auth, dashborad, material, pages)

---
+++ src/app/modules/pages/pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mms-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.scss']
+})
+export class PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
